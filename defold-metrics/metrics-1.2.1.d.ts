354a27e7deaba811f66fe8f06baee894f421f697
thejustinwalsh <contact.me@thejustinwalsh.com>
2021-10-26 01:56:13 +0000

    Deploying to gh-pages from @ ts-defold/library@195b5f5594f31f7cfdc8eaee369d63a4c6159e3c 🚀

---
+++ defold-metrics/metrics-1.2.1.d.ts
@@ -0,0 +1,27 @@
+/// <library version="1.2.1" src="https://github.com/britzl/defold-metrics/archive/refs/tags/1.2.1.zip" />
+/** @noSelfInFile **/
+
+/** @noResolution */
+declare module 'metrics.fps' {
+  /** @noSelf */
+  export interface Metrics {
+    fps: () => number;
+    update: () => void;
+    draw: () => void;
+  }
+
+  export function create(samples?: number, format?: string, position?: string, color?: string): Metrics;
+}
+
+/** @noResolution */
+declare module 'metrics.mem' {
+  /** @noSelf */
+  export interface Metrics {
+    mem: () => number;
+    update: () => void;
+    draw: () => void;
+  }
+
+  export function create(format?: string, position?: string, color?: string): Metrics;
+}
+
