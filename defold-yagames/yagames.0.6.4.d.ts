3f65c9c8897d175041ee831e7bc5cbc61e505965
thejustinwalsh <contact.me@thejustinwalsh.com>
2021-10-30 06:59:28 +0000

    Deploying to gh-pages from @ ts-defold/library@f11ae606b3f54d054c84c9be8534aa793f09d604 🚀

--- defold-yagames/yagames.0.6.4.d.ts
+++ defold-yagames/yagames.0.6.4.d.ts
@@ -49,11 +49,14 @@ declare module 'yagames.yagames' {
 
   export function player_get_stats(keys: Array<string> | null, callback: ApiCallback): void;
 
+  /**
+ * @deprecated Use `player_get_unqiue_id` instead.
+ */
+  export function player_get_id(): string;
+
   export function player_get_unique_id(): string;
 
   export function player_get_name(): string;
-
-  export function player_get_id(): string;
 
   export interface PlayerGetIdsCallback {
     (this: Context, err: string, data: Array<{
@@ -111,4 +114,147 @@ declare module 'yagames.yagames' {
   export function payments_get_catalog(callback: PaymentsCatalogCallback): void;
 
   export function payments_consume_purchase(purchase_token: string, callback: ApiCallback): void;
-}
+
+  //* Leaderboards
+
+  export function leaderboards_init(callback: ApiCallback): void;
+
+  export interface LeaderboardsDescriptionCallback {
+    (this: Context, err: string, data: {
+      appID: string;
+      default: boolean;
+      description: {
+        invert_sort_order: boolean;
+        score_format: {
+          options: {
+            decimal_offset: number;
+          }
+        }
+        type: string;
+      }
+      name: string;
+      title: {
+        en: string;
+        ru: string;
+      }
+    }): void;
+  }
+  export function leaderboards_get_description(leaderboard_name: string, callback: LeaderboardsDescriptionCallback): void;
+
+  export interface LeaderboardsGetPlayerEntryOptions {
+    includeUser: boolean;
+    quantityAround: number;
+    quantityTop: number;
+    getAvatarSrc: "small" | "medium" | "large";
+    getAvatarSrcSet: "small" | "medium" | "large"
+  }
+  export function leaderboards_get_player_entry(leaderboard_name: string, options: LeaderboardsGetPlayerEntryOptions | null, callback: ApiCallback): void;
+
+  export function leaderboards_set_score(leaderboard_name: string, score: number, extra_data: string, callback: ApiCallback): void;
+
+  //* Feedback
+
+  export interface FeedbackCanReviewCallback {
+    (this: Context, err: string, data: {
+      value: boolean;
+      reason: string;
+    }): void;
+  }
+  export function feedback_can_review(callback: FeedbackCanReviewCallback): void;
+
+  export interface FeedbackCanReviewCallback {
+    (this: Context, err: string, data: {
+      feedbackSent: boolean;
+    }): void;
+  }
+  export function feedback_request_review(callback: FeedbackCanReviewCallback): void;
+
+  //* Clipboard
+
+  export function clipboard_write_text(text: string, callback?: ApiCallback): void;
+
+  //* Device Info
+
+  export function device_info_type(): "desktop" | "mobile" | "tablet";
+
+  export function device_info_is_desktop(): boolean;
+
+  export function device_info_is_mobile(): boolean;
+
+  export function device_info_is_tablet(): boolean;
+
+  //* Environment
+
+  export interface EnvironmentResults {
+    app: {
+      id: string;
+    }
+    browser: {
+      lang: string;
+    }
+    i18n: {
+      lang: string;
+      tld: string;
+    }
+    payload?: string;
+  }
+  export function environment(): EnvironmentResults;
+
+  //* Screen
+
+  export function screen_fullscreen_status(): "on" | "off";
+
+  export function screen_fullscreen_request(callback?: ApiCallback): void;
+  
+  export function screen_fullscreen_exit(callback?: ApiCallback): void;
+
+  //* Safe Storage
+
+  export function storage_init(callback: ApiCallback): void;
+
+  export function storage_get_item(key: string): string | null;
+
+  export function storage_set_item(key: string, value: string): void;
+
+  export function storage_remove_item(key: string): void;
+
+  export function storage_clear(): void;
+
+  export function storage_key(n: number): string | null;
+
+  export function storage_length(): number;
+
+  //* Banner Ads
+
+  export function banner_init(callback: ApiCallback): void;
+
+  export interface BannerCreateOptions {
+    stat_id?: number;
+    css_styles?: string;
+    css_class?: string;
+    display?: "none" | "block";
+  }
+  export interface BannerCreateCallback {
+    (this: Context, err: string, data: {
+      product: "direct" | "rtb";
+    }): void;
+  }
+  export function banner_create(rtb_id: string, options: BannerCreateOptions, callback?: BannerCreateCallback): void;
+  
+  export function banner_delete(rtb_id: string): void;
+  
+  export function banner_refresh(rtb_id: string, callback?: BannerCreateCallback): void;
+  
+  export function banner_set(rtb_id: string, property: "stat_id" | "css_styles" | "css_class" | "display", value: string): void;
+
+  //* Sitelock
+  
+  export function add_domain(domain: string): void;
+  
+  export function verify_domain(): boolean;
+  
+  export function get_current_domain(): string;
+  
+  export function is_release_build(): boolean;
+
+ }
