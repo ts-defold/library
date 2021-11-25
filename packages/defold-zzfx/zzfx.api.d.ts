dde5e30aa6dd02f6410228835997f4e148711a7a
Justin Walsh <contact.me@thejustinwalsh.com>
2021-11-25 05:47:00 -0700

    fix: update api (#8)
    * fix: skipping library is now a publish error
    
    * fix(zzfx): pacakge path
    
    * fix: update api

--- packages/defold-zzfx/zzfx.api.d.ts
+++ packages/defold-zzfx/zzfx.api.d.ts
@@ -1,13 +1,15 @@
 /** @noSelfInFile **/
+type ZzFXSample = LuaUserdata & {
+  readonly __index: 'zzfx_ext_sample';
+};
 
 /** @noResolution */
 declare module 'zzfx.api' {
-  export function play(...args: number[]): void;
+export function play(...args: number[]): void;
 
-  export function samplerate(rate: number): void;
+export function samplerate(rate: number): void;
 
-  export function play_sample(...args: number[]): void;
+export function play_sample(sample: ZzFXSample): void;
 
-  export function build_sample(...args: number[]): void;
-
+export function build_sample(...args: number[]): ZzFXSample;
 }
