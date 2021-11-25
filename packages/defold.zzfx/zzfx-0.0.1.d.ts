1680018f678c053ddcbd52747fcfcf12353fc62a
Justin Walsh <contact.me@thejustinwalsh.com>
2021-11-25 03:31:16 -0700

    fix: missing library reference

--- packages/defold.zzfx/zzfx-0.0.1.d.ts
+++ packages/defold.zzfx/zzfx-0.0.1.d.ts
@@ -1,6 +1,2 @@
-/// <library version="1.0.0" src="https://github.com/acme/template/archive/refs/tags/1.0.0.zip" />
+/// <library version="0.0.1" src="https://github.com/thejustinwalsh/defold-zzfx/archive/extension.zip" />
 /// <reference path="./zzfx.d.ts" />
-
-// We can use reference paths to bring in core types that are shared across all versions
-// then only include additional types that are specific to the version of the library here
-// This can be stacked too, each version can import the previous version, or portions of it.
