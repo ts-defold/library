ba18566634441635dc962d7127ace1cadff1c997
Justin Walsh <contact.me@thejustinwalsh.com>
2021-11-25 03:22:28 -0700

    feat: defold-zzfx main

---
+++ packages/defold.zzfx/zzfx-0.0.1.d.ts
@@ -0,0 +1,6 @@
+/// <library version="1.0.0" src="https://github.com/acme/template/archive/refs/tags/1.0.0.zip" />
+/// <reference path="./zzfx.d.ts" />
+
+// We can use reference paths to bring in core types that are shared across all versions
+// then only include additional types that are specific to the version of the library here
+// This can be stacked too, each version can import the previous version, or portions of it.
