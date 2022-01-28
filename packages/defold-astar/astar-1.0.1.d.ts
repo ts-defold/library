b49a731702d7fbb3dc520d32cea936b39e888a19
Justin Walsh <contact.me@thejustinwalsh.com>
2022-01-27 21:16:24 -0700

    fix: defold astar path

--- packages/defold-astar/astar-1.0.1.d.ts
+++ packages/defold-astar/astar-1.0.1.d.ts
@@ -1,2 +1,2 @@
-/// <library version="0.0.1" src="https://github.com/ts-defold/defold-lldebugger/archive/extension.zip" />
+/// <library version="0.0.1" src="https://github.com/selimanac/defold-astar/archive/refs/tags/v1.0.1.zip" />
 /// <reference path="./astar.d.ts" />
