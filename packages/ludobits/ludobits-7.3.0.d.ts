9e399e2b5a0e51492da039b340092772eb7cf362
A K <alex.korsak@gmail.com>
2023-06-06 16:42:16 +0200

    add flow types

--- packages/ludobits/ludobits-7.3.0.d.ts
+++ packages/ludobits/ludobits-7.3.0.d.ts
@@ -1,13 +1,3 @@
 /// <library version="7.3.0" src="https://github.com/britzl/ludobits/archive/refs/tags/7.3.0.zip" />
-/** @noSelfInFile **/
-
-/** @noResolution */
-declare module "ludobits.m.broadcast" {
-  export function register(name: string): void;
-
-  export function unregister(name: string): void;
-
-  export function on_message(message_id: hash, message: any, sender: any): void;
-
-  export function send(message_id: hash, message: any): void;
-}
+/// <reference path="./ludobits.m.broadcast.d.ts" />
+/// <reference path="./ludobits.m.flow.d.ts" />
