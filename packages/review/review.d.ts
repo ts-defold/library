6ca8376f164a85b0c815c7e75d9f166dc16dad05
thinknathan <github@thinknathan.com>
2024-01-31 20:31:51 -0800

    Add review

---
+++ packages/review/review.d.ts
@@ -0,0 +1,18 @@
+/** @noSelfInFile */
+
+/**
+ * Native iOS and Android ratings and reviews window for Defold
+ * @url https://github.com/defold/extension-review
+ * @noResolution
+ */
+declare namespace review {
+	/**
+	 * Open native review/rating popup
+	 */
+	export function request_review(): void;
+
+	/**
+	 * Available only on iOS 10.3+. Android 5.0+ (API 21+) and the Google Play Store has to be installed.
+	 */
+	export function is_supported(): void;
+}
