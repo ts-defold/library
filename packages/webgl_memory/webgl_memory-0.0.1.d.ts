d2b5bb91fa73e997b602f325d67c929534c58374
thinknathan <github@thinknathan.com>
2024-01-31 20:32:43 -0800

    Add webgl_memory

---
+++ packages/webgl_memory/webgl_memory-0.0.1.d.ts
@@ -0,0 +1,2 @@
+/// <library version="0.0.1" src="https://github.com/indiesoftby/defold-webgl-memory/archive/main.zip" />
+/// <reference path="./webgl_memory.d.ts" />
---
+++ packages/webgl_memory/webgl_memory.d.ts
@@ -0,0 +1,48 @@
+/** @noSelfInFile */
+
+/**
+ * A quick and easy way to find out the GPU and CPU memory usage of your Defold game on HTML5 platform.
+ * @url https://github.com/indiesoftby/defold-webgl-memory
+ * @noResolution
+ */
+declare namespace webgl_memory {
+	type memory = {
+		wasmheap: number;
+		buffer: number;
+		renderbuffer: number;
+		total: number;
+		drawingbuffer: number;
+		texture: number;
+	};
+
+	type context = {
+		renderer: string;
+		unmasked_vendor: string;
+		vendor: string;
+		unmasked_renderer: string;
+	};
+
+	type resources = {
+		buffer: number;
+		program: number;
+		shader: number;
+		sampler: number;
+		framebuffer: number;
+		sync: number;
+		query: number;
+		vertexArray: number;
+		transformFeedback: number;
+		renderbuffer: number;
+		texture: number;
+	};
+
+	/**
+	 * Returns a table with memory info data
+	 * @returns {object}
+	 */
+	export function get_info(): {
+		memory: memory;
+		context: context;
+		resources: resources;
+	};
+}
