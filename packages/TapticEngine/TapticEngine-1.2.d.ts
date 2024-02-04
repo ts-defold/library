c79fd915f271398fa8da9bd936b4998190cdf847
Nathan Bolton <github@thinknathan.com>
2024-02-04 05:54:19 +0000

    Adds def_taptic_engine, defold-clipboard, poki-sdk (#40)
    Adds:
    
    -
    [def_taptic_engine](https://github.com/MaratGilyazov/def_taptic_engine)
    - [defold-clipboard](https://github.com/britzl/defold-clipboard)
    - [poki-sdk](https://github.com/defold/extension-poki-sdk)

---
+++ packages/TapticEngine/TapticEngine-1.2.d.ts
@@ -0,0 +1,2 @@
+/// <library version="1.2" src="https://github.com/MaratGilyazov/def_taptic_engine/archive/refs/tags/v1.2.zip" />
+/// <reference path="./TapticEngine.d.ts" />
---
+++ packages/poki-sdk/poki-sdk.d.ts
@@ -0,0 +1,61 @@
+/** @noSelfInFile */
+
+/**
+ * Poki SDK native extension for Defold game engine
+ * @url https://github.com/defold/extension-poki-sdk
+ * @noResolution
+ */
+declare namespace poki_sdk {
+	/**
+	 * Equivalent to the JS SDK's PokiSDK.gameplayStart()
+	 */
+	export function gameplay_start(): void;
+
+	/**
+	 * Equivalent to the JS SDK's PokiSDK.gameplayStop()
+	 */
+	export function gameplay_stop(): void;
+
+	/**
+	 * Equivalent to the JS SDK's PokiSDK.commercialBreak()
+	 * @param {function} callback
+	 */
+	export function commercial_break(callback: () => void): void;
+
+	/**
+	 * Equivalent to the JS SDK's PokiSDK.rewardedBreak()
+	 * @param {function} callback
+	 */
+	export function rewarded_break(callback: () => void): void;
+
+	/**
+	 * Equivalent to the JS SDK's PokiSDK.setDebug(value)
+	 * @param {boolean} is_debug
+	 */
+	export function set_debug(is_debug: boolean): void;
+
+	/**
+	 * Equivalent to the JS SDK's PokiSDK.captureError(error_string)
+	 * @param {string} error
+	 */
+	export function capture_error(error: string): void;
+
+	/**
+	 * Equivalent to the JS SDK's PokiSDK.shareableURL({}).then(url => {})
+	 * @param {table} params
+	 * @param {function} callback
+	 */
+	export function shareable_url(params: {}, callback: () => void): void;
+
+	/**
+	 * Equivalent to the JS SDK's PokiSDK.getURLParam('id')
+	 * @param {string} key
+	 * @returns {string}
+	 */
+	export function get_url_param(key: string): string;
+
+	/**
+	 * @deprecated since version 1.5.0
+	 */
+	export function is_ad_blocked(): boolean;
+}
