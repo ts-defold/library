9e820ed4b78b9e88e0447a4561efc7d11d9c0565
thinknathan <github@thinknathan.com>
2024-02-21 20:57:11 -0800

    refactor lib names and filenames

--- packages/extension-nativetext/extension-nativetext-0.0.1.d.ts
+++ packages/extension-nativetext/extension-nativetext-0.0.1.d.ts
@@ -1,2 +1,2 @@
 /// <library version="0.0.1" src="https://github.com/Lerg/extension-nativetext/archive/master.zip" />
-/// <reference path="./extension-nativetext.d.ts" />
+/// <reference path="./nativetext.d.ts" />
--- packages/jstodef_/jstodef-2.0.0.d.ts
+++ packages/jstodef_/jstodef-2.0.0.d.ts
@@ -1,2 +1,2 @@
 /// <library version="2.0.0" src="https://github.com/AGulev/jstodef/archive/refs/tags/2.0.0.zip" />
-/// <reference path="./JsToDef.d.ts" />
+/// <reference path="./jstodef.d.ts" />
