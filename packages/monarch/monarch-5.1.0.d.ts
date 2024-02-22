1db3d4bbb785d5cf465a61ef158a8f9d1c0f61ee
thinknathan <github@thinknathan.com>
2024-02-21 17:17:54 -0800

    Add more monarch libraries

--- packages/monarch/monarch-5.1.0.d.ts
+++ packages/monarch/monarch-5.1.0.d.ts
@@ -1,2 +1,4 @@
 /// <library version="5.1.0" src="https://github.com/britzl/monarch/archive/refs/tags/5.1.0.zip" />
-/// <reference path="./monarch.d.ts" />
+/// <reference path="./monarch.monarch.d.ts" />
+/// <reference path="./monarch.transitions.gui.d.ts" />
+/// <reference path="./monarch.transitions.easings.d.ts" />
