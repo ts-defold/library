46b969338a3ba8a3010d2fee1d22315b886910cb
Nathan Bolton <github@thinknathan.com>
2024-02-22 05:32:26 +0000

    Refactor (#49)
    - Update folder and library names to match convention
    - Change DefSave return type `any` to `unknown`
    - Fix version in astar

--- packages/extension-iap/iap-5.1.0.d.ts
+++ packages/extension-iap/iap-5.1.0.d.ts
@@ -1,2 +1,2 @@
 /// <library version="5.1.0" src="https://github.com/defold/extension-iap/archive/refs/tags/5.1.0.zip" />
-/// <reference path="./extension-iap.d.ts" />
+/// <reference path="./iap.d.ts" />
--- packages/extension-safearea/safearea-1.4.0.d.ts
+++ packages/extension-safearea/safearea-1.4.0.d.ts
@@ -1,2 +1,2 @@
 /// <library version="1.4.0" src="https://github.com/defold/extension-safearea/archive/refs/tags/1.4.0.zip" />
-/// <reference path="./extension-safearea.d.ts" />
+/// <reference path="./safearea.d.ts" />
