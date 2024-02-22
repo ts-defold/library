14e1112156d0fc45275b8d6007eaeb8d5a4cc49a
thinknathan <github@thinknathan.com>
2024-02-21 21:11:46 -0800

    Fix library version

--- packages/defold-astar/astar-1.0.1.d.ts
+++ packages/defold-astar/astar-1.0.1.d.ts
@@ -1,2 +1,2 @@
-/// <library version="0.0.1" src="https://github.com/selimanac/defold-astar/archive/refs/tags/v1.0.1.zip" />
+/// <library version="1.0.1" src="https://github.com/selimanac/defold-astar/archive/refs/tags/v1.0.1.zip" />
 /// <reference path="./astar.d.ts" />
