3455c902973632fc15ec44462489282734484f7a
thinknathan <github@thinknathan.com>
2024-02-24 14:22:51 -0800

    Fix version

--- packages/extension-spine/spine-2.1.4.d.ts
+++
@@ -1,3 +0,0 @@
-/// <library version="2.1.4" src="https://github.com/defold/extension-spine/archive/refs/tags/2.14.4.zip" />
-/// <reference path="spine.d.ts" />
-/// <reference path="gui.d.ts" />
---
+++ packages/extension-spine/spine-2.14.4.d.ts
@@ -0,0 +1,3 @@
+/// <library version="2.14.4" src="https://github.com/defold/extension-spine/archive/refs/tags/2.14.4.zip" />
+/// <reference path="spine.d.ts" />
+/// <reference path="gui.d.ts" />
