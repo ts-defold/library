ed5faeff4fc11dce48d6f8a4f8fc5dd17a28626e
thinknathan <github@thinknathan.com>
2024-02-25 05:18:49 +0000

    Deploying to gh-pages from @ ts-defold/library@994c02fcad9a9ee41dea9767472ba54571034550 🚀

--- defold-orthographic/orthographic-3.3.0.d.ts
+++ defold-orthographic/orthographic-3.3.0.d.ts
@@ -1,4 +1,4 @@
-/// <library version="3.1.3" src="https://github.com/britzl/defold-orthographic/archive/refs/tags/3.1.3.zip" />
+/// <library version="3.3.0" src="https://github.com/britzl/defold-orthographic/archive/refs/tags/3.3.0.zip" />
 /** @noSelfInFile **/
 
 /** @noResolution */
@@ -303,4 +303,18 @@ declare module 'orthographic.camera' {
 	export const MSG_USE_PROJECTION: hash;
 	export const MSG_VIEWPORT: hash;
 	export const MSG_ENABLE: hash;
+}
+
+
+/** @noResolution */
+declare module 'orthographic.camera' {
+	/**
+	 * Translate world coordinates to window coordinates, based on the view and projection of the camera.
+	 * @param camera_id nil for the first camera
+	 * @param world World coordinates to convert
+	 */
+	export function world_to_window(
+		camera_id: hash | url | undefined,
+		world: vmath.vector3,
+	): vmath.vector3;
 }
