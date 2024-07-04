c5ff16e467dc634b8f3547f1fef3dadf7e5a016e
thinknathan <github@thinknathan.com>
2024-07-03 17:56:46 -0700

    feat: add simple packages

---
+++ packages/LuaScriptInstance/lua_script_instance-0.1.1.d.ts
@@ -0,0 +1,2 @@
+/// <library version="0.1.1" src="https://github.com/DanEngelbrecht/LuaScriptInstance/archive/refs/tags/0.1.1.zip" />
+/// <reference path="./lua_script_instance.d.ts" />
---
+++ packages/extension-videoplayer-native/videoplayer-1.0.1.d.ts
@@ -0,0 +1,2 @@
+/// <library version="1.0.1" src="https://github.com/defold/extension-videoplayer-native/archive/refs/tags/1.0.1.zip" />
+/// <reference path="./videoplayer.d.ts" />
