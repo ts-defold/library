62fc2921465744d087a2f34f0d3af4fa368afe6a
thinknathan <github@thinknathan.com>
2024-07-05 00:11:10 +0000

    Deploying to gh-pages from @ ts-defold/library@d8406749c15f9072b4a9870da7318995646a21c2 🚀

--- defold-prng/prng-1.1.0.d.ts
+++ defold-prng/prng-1.1.0.d.ts
@@ -1,4 +1,4 @@
-/// <library version="1.0.2" src="https://github.com/thinknathan/defold-prng/archive/refs/tags/v1.0.2.zip" />
+/// <library version="1.1.0" src="https://github.com/thinknathan/defold-prng/archive/refs/tags/v1.1.0.zip" />
 /** @noSelfInFile */
 
 /**
@@ -22,4 +22,37 @@ declare namespace prng {
 	 * Requests a floating point number between 0 and 1.
 	 */
 	export function rand_float(): number;
+}
+
+
+/** @noSelfInFile */
+
+/**
+ * Defold psuedo-random number generator extension using the Mulberry32 method.
+ */
+declare namespace prng {
+	/**
+	 * Flips a coin.
+	 * @returns 0 or 1
+	 * @since 1.1.0
+	 */
+	export function coin(): 0 | 1;
+
+	/**
+	 * Get a card suit.
+	 * @returns 0, 1, 2 or 3
+	 * @since 1.1.0
+	 */
+	export function suit(): 0 | 1 | 2 | 3;
+
+	/**
+	 * Roll one or more dice.
+	 * @returns result of dice roll
+	 * @since 1.1.0
+	 */
+	export function dice(
+		num_dice: number,
+		num_sides: number,
+		modifier?: number,
+	): number;
 }
---
+++ defold-prng/prng-1.2.0.d.ts
@@ -0,0 +1,81 @@
+/// <library version="1.2.0" src="https://github.com/thinknathan/defold-prng/archive/refs/tags/v1.2.0.zip" />
+/// <library version="1.1.0" src="https://github.com/thinknathan/defold-prng/archive/refs/tags/v1.1.0.zip" />
+/** @noSelfInFile */
+
+/**
+ * Defold psuedo-random number generator extension using the Mulberry32 method.
+ * @see {@link https://github.com/thinknathan/defold-prng|Github Source}
+ */
+declare namespace prng {
+	/**
+	 * Sets `s` as the seed for the pseudo-random number generator. The resulting sequences of numbers will be identical when the seeds are identical.
+	 * @param s Must be an integer.
+	 */
+	export function set_seed(s: number): void;
+
+	/**
+	 * Requests an integer between 0 and `max`.
+	 * @param max
+	 */
+	export function rand_int(max: number): number;
+
+	/**
+	 * Requests a floating point number between 0 and 1.
+	 */
+	export function rand_float(): number;
+}
+
+
+/** @noSelfInFile */
+
+/**
+ * Defold psuedo-random number generator extension using the Mulberry32 method.
+ */
+declare namespace prng {
+	/**
+	 * Flips a coin.
+	 * @returns 0 or 1
+	 * @since 1.1.0
+	 */
+	export function coin(): 0 | 1;
+
+	/**
+	 * Get a card suit.
+	 * @returns 0, 1, 2 or 3
+	 * @since 1.1.0
+	 */
+	export function suit(): 0 | 1 | 2 | 3;
+
+	/**
+	 * Roll one or more dice.
+	 * @returns result of dice roll
+	 * @since 1.1.0
+	 */
+	export function dice(
+		num_dice: number,
+		num_sides: number,
+		modifier?: number,
+	): number;
+}
+
+
+/** @noSelfInFile */
+
+/**
+ * Defold psuedo-random number generator extension using the Mulberry32 method.
+ */
+declare namespace prng {
+	/**
+	 * Rolls twice and takes the higher result. More likely to return 1 than 0.
+	 * @returns 0 or 1
+	 * @since 1.2.0
+	 */
+	export function advantage(): 0 | 1;
+
+	/**
+	 * Rolls twice and takes the lower result. More likely to return 0 than 1.
+	 * @returns 0 or 1
+	 * @since 1.2.0
+	 */
+	export function disadvantage(): 0 | 1;
+}
