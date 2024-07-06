fdecf859306091d33d5c154765e6d0453d311952
thinknathan <github@thinknathan.com>
2024-07-05 17:04:52 -0700

    feat: add defold-uuid definitions

---
+++ packages/defold-uuid4/uuid4-0.0.1.d.ts
@@ -0,0 +1,2 @@
+/// <library version="0.0.1" src="https://github.com/selimanac/defold-uuid4/archive/refs/heads/master.zip" />
+/// <reference path="./uuid4.d.ts" />
---
+++ packages/defold-uuid4/uuid4.d.ts
@@ -0,0 +1,9 @@
+/** @noSelfInFile */
+
+/**
+ * Defold uuid4 generator native extension 
+ * @see {@link https://github.com/selimanac/defold-uuid4|Github Source}
+ */
+declare namespace uuid4 {
+	export function generate(): string;
+}
