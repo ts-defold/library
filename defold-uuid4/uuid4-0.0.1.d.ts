812ddfed395c49abf67a0af726dec84a68506931
thinknathan <github@thinknathan.com>
2024-07-06 00:06:22 +0000

    Deploying to gh-pages from @ ts-defold/library@da2443e2b12e86b3699fb1d5042750d0d74a837b 🚀

---
+++ defold-uuid4/uuid4-0.0.1.d.ts
@@ -0,0 +1,10 @@
+/// <library version="0.0.1" src="https://github.com/selimanac/defold-uuid4/archive/refs/heads/master.zip" />
+/** @noSelfInFile */
+
+/**
+ * Defold uuid4 generator native extension 
+ * @see {@link https://github.com/selimanac/defold-uuid4|Github Source}
+ */
+declare namespace uuid4 {
+	export function generate(): string;
+}
