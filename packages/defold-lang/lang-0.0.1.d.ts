f7ef5fb6dc71f048d7a11a3092a87ef8e19e865e
Nathan Bolton <github@thinknathan.com>
2024-08-17 18:50:35 +0000

    feat: add def-setv (#80)

--- packages/defold-lang/lang-0.0.1.d.ts
+++ packages/defold-lang/lang-0.0.1.d.ts
@@ -1,3 +1,2 @@
-/* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <library version="0.0.1" src="https://github.com/Insality/defold-lang/archive/refs/tags/1.zip" />
 /// <reference path="lang.lang.d.ts" />
--- packages/extension-videoplayer-mpeg/mpeg-0.0.1.d.ts
+++ packages/extension-videoplayer-mpeg/mpeg-0.0.1.d.ts
@@ -1,3 +1,2 @@
-/* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <library version="0.0.1" src="https://github.com/defold/extension-videoplayer-mpeg/archive/refs/heads/master.zip" />
 /// <reference path="mpeg.d.ts" />
