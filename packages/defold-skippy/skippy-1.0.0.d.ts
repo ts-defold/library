a3954ff54141f5ffee828ad9fa5a9f17ab95840a
Nathan Bolton <github@thinknathan.com>
2024-09-08 21:43:04 +0000

    feat: add Defold Skippy Extension (#82)
    [Defold Skippy Extension](https://github.com/NaakkaDev/defold-skippy)

---
+++ packages/defold-skippy/skippy-1.0.0.d.ts
@@ -0,0 +1,2 @@
+/// <library version="1.0.0" src="https://github.com/NaakkaDev/defold-skippy/archive/refs/tags/1.0.zip" />
+/// <reference path="./skippy.d.ts" />
---
+++ packages/defold-skippy/skippy.d.ts
@@ -0,0 +1,22 @@
+/** @noSelfInFile */
+
+/**
+ * Steps counter & detector extension for the Defold game engine. 
+ * @see {@link https://github.com/NaakkaDev/defold-skippy|Github Source}
+ */
+declare namespace skippy {
+	/** Start the sensor listeners. */
+	export function start(): void;
+	/** Stop the sensor listeners. */
+	export function stop(): void;
+	/**
+	 * All the steps since the device was powered on.
+	 * For Android this means the TYPE_STEP_COUNTER sensor data.
+	 */
+	export function get_steps(): number;
+	/**
+	 * Amount of steps detected since the app was started.
+	 * For Android this means the TYPE_STEP_DETECTOR sensor data.
+	 */
+	export function get_steps_detector(): number; // TYPE_STEP_DETECTOR
+}
